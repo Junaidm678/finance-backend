7792f15581e50065d296c74a1f401937fd0db57f
Junaidm678 <123249168+Junaidm678@users.noreply.github.com>
2026-04-05 23:49:17 +0530

    Update roleMiddleware.js

--- middleware/roleMiddleware.js
+++ middleware/roleMiddleware.js
@@ -1,6 +1,6 @@
 const allowedRoles = (...roles) => {
   return (req, res, next) => {
-    // check if role is allowed
+    // check if role is allowed or not
     if (!roles.includes(req.user.role)) {
       return res.status(403).json({ message: "Access denied" });
     }
